52ec203260e07e202fcb135150def9e03e9d9f28
Steven Mackey <stevester94@gmail.com>
2019-12-03 21:30:37 -0800

    This is what was in the presentation

--- parse_results.py
+++ parse_results.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
 print("res has the results list")
 print("pp.pprint(__)")
 print('pp.pprint( [e for e in res if e["Protocol"] == "tcp" and "short" in e["Test Name"]])')
+print('pp.pprint( [e for e in res if e["Protocol"] == "tcp" and "GIMP" in e["Test Name"]])')
 
